407c3deac7c7e49f36ab0d5153c33afd40a5c36f
Huitzitzili Domingo <42973573+HuitziDomingo@users.noreply.github.com>
2021-07-28 12:01:11 -0500

    segundo commit

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NewOrder, Menu, DetailPlate, FormPlate, OrderSummary, ProgressRequested } from './views'
+import { StatusBar } from 'react-native';
+import FirebaseState from './context/firebase/firebaseState' //Importar state de contexto
 
 const Stack = createStackNavigator()
 
@@ -13,11 +15,19 @@ export default () => {
 
   return (
     <>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="NuvaOrden" component={NewOrder} options={{title: "Nueva Orden"}} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <FirebaseState>
+        <NavigationContainer>
+          <StatusBar backgroundColor="#000" barStyle="light-content" />
+          <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#000' }, headerTitleStyle: { fontWeight: 'bold', color: '#fff' } }}>
+            <Stack.Screen name="NuvaOrden" component={NewOrder} options={{ title: "Nueva Orden" }} />
+            <Stack.Screen name="Menu" component={Menu} options={{ title: "Menu" }} />
+            <Stack.Screen name="DetallePlatillo" component={DetailPlate} options={{ title: "Detalle Platillo" }} />
+            <Stack.Screen name="OrdenarPlatillo" component={FormPlate} options={{ title: "Ordenar Platillo" }} />
+            <Stack.Screen name="ResumenPedido" component={OrderSummary} options={{ title: "Resumen PEdido" }} />
+            <Stack.Screen name="ProgresoPedido" component={ProgressRequested} options={{ title: "Progreso de Pedido" }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FirebaseState>
     </>
   )
 }
---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDqpzehLuO74oNC5XwBL4w_dUPm8l_lrrA",
+    authDomain: "restaurant-65c6d.firebaseapp.com",
+    projectId: "restaurant-65c6d",
+    storageBucket: "restaurant-65c6d.appspot.com",
+    messagingSenderId: "96230326089",
+    appId: "1:96230326089:web:32114666421c4ded90a498",
+    measurementId: "G-8KHFFEG6N7"
+};
+
+export default firebaseConfig
